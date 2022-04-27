7eebd08842465884eff6077c25dd21814d9651ce
ytchao0234 <nd68319@gmail.com>
2022-04-27 10:41:21 +0800

    Initial Commit

---
+++ Match3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Match3
+//
+//  Created by FanRende on 2022/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Match3/Match3App.swift
@@ -0,0 +1,17 @@
+//
+//  Match3App.swift
+//  Match3
+//
+//  Created by FanRende on 2022/4/27.
+//
+
+import SwiftUI
+
+@main
+struct Match3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
